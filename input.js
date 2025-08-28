77068bd1ae2d159f4c4f23d3284f5c0d72195b5e
Mahdi Hasan <hasanmahdi6000@gmail.com>
2025-08-28 17:16:04 +0600

    Add JS For Calling Button And Coin Counter

--- input.js
+++ input.js
@@ -24,6 +24,18 @@ cardContainer.addEventListener('click', function (e) {
       e.target.classList.remove('text-red-500');
     }
   }
+  const callButton = e.target.closest('.call-btn');
+  if (callButton) {
+    const serviceName = callButton.parentNode.parentNode.children[1].children[1].textContent;
+    const serviceNumber = callButton.parentNode.parentNode.children[2].children[0].textContent;
+
+    const coinCounter = Number(getElement('coin-counter').textContent);
+    if (coinCounter >= 20) {
+      getElement('coin-counter').textContent = coinCounter - 20;
+    } else {
+      alert('⚠️ Insufficient balance. A minimum of 20 coins is needed to place a call.');
+      return;
+    }
+    alert(`📞 Calling ${serviceName} Service ${serviceNumber}...`);
+  }
 });
-
-
